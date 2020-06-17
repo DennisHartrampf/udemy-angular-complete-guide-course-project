18381f3a7fdb0157a617443236808f01a0d65098
dennis.hartrampf <dennis.hartrampf@postcon.de>
2020-06-17 11:06:08 +0200

    Layout of a recipe list item plus one dummy recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Spaghetti Aglio e Olio', 'Lecker!', 
+      'https://img.chefkoch-cdn.de/rezepte/1452391250171274/bilder/1236586/crop-960x640/spaghetti-aglio-olio-e-peperoncino.jpg')
+  ];
   
   constructor() { }
 
