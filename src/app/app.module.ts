783495bb5196c0c30550c03c8611ba1753dcbf23
dennis.hartrampf <dennis.hartrampf@postcon.de>
2020-06-17 10:20:38 +0200

    A little bit more structure/layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
@@ -15,7 +15,7 @@ import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.com
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent,
+    ShoppingEditComponent,
     RecipesComponent,
     RecipeListComponent,
     RecipeItemComponent,
