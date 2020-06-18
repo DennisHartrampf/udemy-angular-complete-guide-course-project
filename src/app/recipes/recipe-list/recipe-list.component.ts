5ec7b55764d23583676ef4332a74c4aa4e9f4fb6
dennis.hartrampf <dennis.hartrampf@postcon.de>
2020-06-18 14:57:00 +0200

    Move recipe content to it's own component, passing the data from recipe-list to recipe-item via property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,10 +9,10 @@ import {Recipe} from "../recipe.model";
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('Spaghetti Aglio e Olio', 'Lecker!', 
+    new Recipe('Spaghetti Aglio e Olio', 'Sehr lecker!', 
       'https://img.chefkoch-cdn.de/rezepte/1452391250171274/bilder/1236586/crop-960x640/spaghetti-aglio-olio-e-peperoncino.jpg'),
-    new Recipe('Spaghetti Aglio e Olio', 'Lecker!', 
-      'https://img.chefkoch-cdn.de/rezepte/1452391250171274/bilder/1236586/crop-960x640/spaghetti-aglio-olio-e-peperoncino.jpg'),
+    new Recipe('Pizza Funghi', 'Lecker!', 
+      'https://www.bella-cucina.de/wp-content/uploads/2016/11/pizza-funghi.jpg'),
   ];
   
   constructor() { }
