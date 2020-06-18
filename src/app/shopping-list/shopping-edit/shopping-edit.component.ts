b7805243cd0608c344a5d6dde1247b19b11ea9a1
dennis.hartrampf <dennis.hartrampf@postcon.de>
2020-06-18 16:44:55 +0200

    Make new ingredients addable - again using custom events and @ViewChild this time to get data out of a form (alternatively I could pass the data out of the form as parameters) Needed to add FormsModule, because otherwise a button with type submit would reload the page

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingEditComponent implements OnInit {
 
+  @Output('ingredientAdded') ingredientEventEmitter = new EventEmitter<Ingredient>();
+  
+  @ViewChild('nameInput') nameInputReference: ElementRef;
+  @ViewChild('amountInput') amountInputReference: ElementRef;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddIngredient() {
+    const name = this.nameInputReference.nativeElement.value;
+    const amount = this.amountInputReference.nativeElement.value;
+    this.ingredientEventEmitter.emit(new Ingredient(name, amount));
+  }
 }
