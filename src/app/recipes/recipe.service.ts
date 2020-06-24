48bc4f1ed7f10d9f49e040d05d60f84117a47fc9
dennis.hartrampf <dennis.hartrampf@postcon.de>
2020-06-24 17:14:12 +0200

    Recipe now has ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +10,23 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   
   private recipes: Recipe[] = [
-    new Recipe('Spaghetti Aglio e Olio', 'Sehr lecker!',
-      'https://img.chefkoch-cdn.de/rezepte/1452391250171274/bilder/1236586/crop-960x640/spaghetti-aglio-olio-e-peperoncino.jpg'),
-    new Recipe('Pizza Funghi', 'Lecker!',
-      'https://www.bella-cucina.de/wp-content/uploads/2016/11/pizza-funghi.jpg'),
+    new Recipe('Spaghetti Aglio e Olio', 
+      'Sehr lecker!',
+      'https://img.chefkoch-cdn.de/rezepte/1452391250171274/bilder/1236586/crop-960x640/spaghetti-aglio-olio-e-peperoncino.jpg',
+      [
+        new Ingredient('Spaghetti', 500),
+        new Ingredient('Garlic', 5),
+        new Ingredient('Olive Oil', 150),
+      ]),
+    new Recipe('Pizza Funghi', 
+      'Lecker!',
+      'https://www.bella-cucina.de/wp-content/uploads/2016/11/pizza-funghi.jpg',
+      [
+        new Ingredient('Dough', 1),
+        new Ingredient('Tomatoes', 2),
+        new Ingredient('Mushrooms', 10),
+        new Ingredient('Cheese', 1),
+      ]),
   ];
   
   getRecipes() {
